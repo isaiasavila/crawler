e2d441f07bbe526622699588198b2dcb83f3da8c
isaiasavila <84739990+isaiasavila@users.noreply.github.com>
2021-12-01 21:40:44 -0300

    Versão 1,5/3

--- crawler.py
+++ crawler.py
@@ -27,11 +27,47 @@ class Robo():
             
             # self.titulo = self.navegador.title
             # elemento = self.navegador.find_element_by_tag_name('name')
-            self.navegador.find_element_by_xpath('//*[@id="yfin-usr-qry"]').click()
-            # self.navegador.find_element_by_xpath('//*[@id="screener-criteria"]/div[2]/div[1]/div[1]/div[1]/div/div[2]/ul/li[5]/div/div[1]/svg').click()
-            # self.navegador.find_element_by_xpath('//*[@id="screener-criteria"]/div[2]/div[1]/div[1]/div[1]/div/div[2]/ul/li[1]/button/svg/path').click()
-            # self.navegador.find_element_by_xpath('//*[@id="screener-criteria"]/div[2]/div[1]/div[1]/div[1]/div/div[2]/ul/li[2]/div/div/svg').click()
+            # self.navegador.find_element_by_xpath('//*[@id="yfin-usr-qry"]').click()
+            # self.navegador.find_element_by_xpath('//*[@id="yfin-usr-qry"]').send_keys('Lulu Chiang')
+            # self.navegador.find_element_by_xpath('//*[@id="header-desktop-search-button"]').click()
+            # svg
+            # self.navegador.find_element_by_xpath('//*[@id="screener-criteria"]/div[2]/div[1]/div[1]/div[1]/div/div[2]/ul/li/div/div').click()
+            # div
+            # self.navegador.find_element_by_xpath('//*[@id="screener-criteria"]/div[2]/div[1]/div[1]/div[1]/div/div[2]/ul/li/div/div').click()
+            # Limpa a seleção do país padrão
+            self.navegador.find_element_by_xpath('//*[@id="screener-criteria"]/div[2]/div[1]/div[1]/div[1]/div/div[2]/ul/li[1]/button').click()
+            # Habilita a seleção dos países desejados
+            self.navegador.find_element_by_xpath('//*[@id="screener-criteria"]/div[2]/div[1]/div[1]/div[1]/div/div[2]/ul').click()
+            # paises = ('Argentina', 'Austria', Australia', 'Belgium', 'Brazil', 'Canada', 'Switzerland', 'Chile', 'China', 'Czech Republic', 'Germany', 'Denmark', 'Estonia', 'Egypt', 'Spain', 'Finland', 'France', 'United Kingdom', 'Greece', 'Hong Kong', 'Hungary', 'Indonesia', 'Ireland', 'Israel, 'India', 'Iceland', 'Japan', 'South Korea', 'Kuwait', 'Sri Lanka', 'Lithuania', 'Latvia', 'Mexico', 'Malaysia', 'Netherlands', 'Norway', 'New Zealand', 'Peru', 'Philippines', 'Pakistan', 'Poland', 'Portugal', 'Qatar','Russia', 'Saudi Arabia', 'Sweden', 'Singapore', 'Suriname', 'Thailand', 'Turkey', 'Taiwan', 'United States', 'Venezuela', 'Vietnam', 'South Africa')
+            # paises = ('Austria','Argentina','Australia')
+            # Tupla contendo os check-boxes dos países que serão selecionados
+            checks = ('//*[@id="dropdown-menu"]/div/div[2]/ul/li[2]/label/input','//*[@id="dropdown-menu"]/div/div[2]/ul/li/label/input','//*[@id="dropdown-menu"]/div/div[2]/ul/li[3]/label/input')
+            # Laço de repetição para a seleção dos países desejados (region)
+            for cont in range(len(checks)):
+                # Tupla servindo como parâmetro de seleção
+                self.navegador.find_element_by_xpath(checks[cont]).click()
+            
+            self.navegador.implicitly_wait(10)
+            elemento = self.navegador.find_element_by_xpath('//*[@id="screener-criteria"]/div[2]/div[1]/div[3]/button[1]')
+            E2 = self.navegador.find_element_by_xpath('//*[@id="screener-criteria"]/div[2]/div[1]/div[3]/button[1]/span')
+            E2.click()
+            elemento.click()
+            
+            # element = self.navegador.find_element_by_xpath('//*[@id="screener-criteria"]/div[2]/div[1]')
+            # element.send_keys("", Keys.ESC)
+            # self.navegador.find_element_by_xpath('//*[@id="screener-criteria"]/div[2]/div[1]').send_keys(Keys.ESC)
+            
+            # Desmarca as opções que não serão utilizadas
+            # self.navegador.find_element_by_xpath('//*[@id="screener-criteria"]/div[2]/div[1]/div[1]/div[1]/div/div[2]/ul').click()
+            
+            # self.navegador.find_element_by_xpath('//*[@id="dropdown-menu"]/button/svg').click()
+            # self.navegador.implicitly_wait(15)
+            # Clique do botão de mostrar resultados
             # self.navegador.find_element_by_xpath('//*[@id="screener-criteria"]/div[2]/div[1]/div[3]/button[1]').click()
+                # self.navegador.find_element_by_xpath('//*[@id="screener-criteria"]/div[2]/div[1]/div[1]/div[1]/div/div[2]/ul/li[5]/div/div[1]/svg').click()
+                # self.navegador.find_element_by_xpath('//*[@id="screener-criteria"]/div[2]/div[1]/div[1]/div[1]/div/div[2]/ul/li[1]/button/svg/path').click()
+                # self.navegador.find_element_by_xpath('//*[@id="screener-criteria"]/div[2]/div[1]/div[1]/div[1]/div/div[2]/ul/li[2]/div/div/svg').click()
+                # self.navegador.find_element_by_xpath('//*[@id="screener-criteria"]/div[2]/div[1]/div[3]/button[1]').click()
 
         except:
             self.driver.close()
